84e0f73c91f1a3964bcbaa71eee8ec2c53c487b1
Nguyen Quang Ngoc <quangngoc.07012002@gmail.com>
2024-11-15 23:39:55 +0700

    Notification Controller

--- lib/controller/notification_controller.dart
+++ lib/controller/notification_controller.dart
@@ -1,35 +1,29 @@
 import 'package:get/get.dart';
-import 'package:stomp_dart_client/stomp_dart_client.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 
+import '../services/websocket_service.dart';
+
 class NotificationController extends GetxController {
-  StompClient? stompClient;
-  final messages = <String>[].obs; // Danh sách tin nhắn dùng GetX
+  final WebSocketService _webSocketService = Get.find();
+  final messages = <String>[].obs;
 
   @override
   void onInit() {
     super.onInit();
-    connect(); // Kết nối WebSocket khi khởi tạo controller
-  }
-
-  // Hàm xử lý khi kết nối thành công
-  void onConnect(StompFrame frame) {
-    print('Connected to WebSocket server');
-    stompClient?.subscribe(
-      destination: '/queue/notifications/123',
-      callback: (StompFrame frame) {
+    _webSocketService.subscribe(
+      '/queue/notifications/123',
+          (frame) {
         if (frame.body != null) {
           print('Received message: ${frame.body}');
-          messages.add(frame.body!); // Thêm tin nhắn vào danh sách
-          showNotificationDialog(frame.body!); // Hiển thị dialog thông báo
+          messages.add(frame.body!);
+          showNotificationDialog(frame.body!);
         }
       },
     );
   }
 
   void showNotificationDialog(String message) {
-    // Hiển thị dialog thông báo sử dụng GetX
     Get.defaultDialog(
       title: "Notification",
       content: Text(message),
@@ -37,30 +31,5 @@ class NotificationController extends GetxController {
       confirmTextColor: Colors.white,
       onConfirm: () => Get.back(),
     );
-  }
-
-  // Kết nối WebSocket
-  void connect() {
-    stompClient = StompClient(
-      config: StompConfig.sockJS(
-        url: 'http://10.0.2.2:8080/nserve/ws',
-        onConnect: onConnect,
-        onWebSocketError: (error) => print('WebSocket Error: $error'),
-        onStompError: (error) => print('STOMP Error: $error'),
-        onDisconnect: (frame) => print('Disconnected from WebSocket server'),
-      ),
-    );
-    stompClient?.activate();
-  }
-
-  // Ngắt kết nối
-  void disconnect() {
-    stompClient?.deactivate();
-  }
-
-  @override
-  void onClose() {
-    disconnect(); // Ngắt kết nối khi controller bị hủy
-    super.onClose();
   }
 }
